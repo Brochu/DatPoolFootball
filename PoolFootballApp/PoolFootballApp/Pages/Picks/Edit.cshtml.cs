bb29bb3714c0fc147dc64583f7667c11cda9a7be
Brochu <alexbrochu1@gmail.com>
2019-03-02 21:01:53 -0500

    Avancer sur le WIP de picks/edit

--- PoolFootballApp/PoolFootballApp/Pages/Picks/Edit.cshtml.cs
+++ PoolFootballApp/PoolFootballApp/Pages/Picks/Edit.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -11,73 +12,90 @@ using PoolFootballApp.Models;
 
 namespace PoolFootballApp.Pages.Picks
 {
-    public class EditModel : PageModel
-    {
-        private readonly NFLContext _context;
+	public class EditModel : PageModel
+	{
+		private readonly NFLContext _context;
 		private readonly IConfiguration _config;
+		private readonly UserManager<IdentityUser> _userManager;
 
-        public EditModel(NFLContext context, IConfiguration config)
-        {
-            _context = context;
+		public EditModel(NFLContext context, IConfiguration config, UserManager<IdentityUser> userManager)
+		{
+			_context = context;
 			_config = config;
-        }
+			_userManager = userManager;
+		}
 
-        [BindProperty]
-        public Pick Pick { get; set; }
+		[BindProperty]
+		public Pick Pick { get; set; }
 
-        public async Task<IActionResult> OnGetAsync()
-        {
+		public async Task<IActionResult> OnGetAsync()
+		{
 			int season = _config.GetValue<int>("Values:CurrentSeason");
 			// TEMP VALUE FOR TESTING
 			int week = 1;
+			string userId = _userManager.GetUserId(User);
 
-			int[] matchIds = await _context.Matches
+			bool addedAny = false;
+			List<int> matchIds = new List<int>();
+			await _context.Matches
 				.Where(m => m.Season == season && m.Week == week)
+				.OrderBy(m => m.WeekDay)
+				.ThenBy(m => m.StartTime)
 				.Select(m => m.Id)
-				.ToArrayAsync();
+				.ForEachAsync(id =>
+				{
+					matchIds.Add(id);
+					if (!_context.Picks.Any(p => p.MatchId == id && p.UserId.Equals(userId)))
+					{
+						_context.Picks.Add(new Pick() { MatchId = id, UserId = userId });
+						addedAny = true;
+					}
+				});
 
-            Pick = await _context.Picks
-                .Include(p => p.Match).FirstOrDefaultAsync(m => m.Id == id);
+			if (addedAny)
+			{
+				_context.SaveChanges();
+			}
 
-            if (Pick == null)
-            {
-                return NotFound();
-            }
-           ViewData["MatchId"] = new SelectList(_context.Matches, "Id", "Id");
-            return Page();
-        }
+			// SELECT ALL THE PICKS FOR THE GIVEN WEEK
+			//Pick = await _context.Picks
+			//	.Include(p => p.Match).FirstOrDefaultAsync(m => m.Id == id);
 
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
+		   ViewData["MatchId"] = new SelectList(_context.Matches, "Id", "Id");
+			return Page();
+		}
 
-            _context.Attach(Pick).State = EntityState.Modified;
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!PickExists(Pick.Id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+			_context.Attach(Pick).State = EntityState.Modified;
 
-            return RedirectToPage("./Index");
-        }
+			try
+			{
+				await _context.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!PickExists(Pick.Id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
 
-        private bool PickExists(int id)
-        {
-            return _context.Picks.Any(e => e.Id == id);
-        }
-    }
+			return RedirectToPage("./Index");
+		}
+
+		private bool PickExists(int id)
+		{
+			return _context.Picks.Any(e => e.Id == id);
+		}
+	}
 }
