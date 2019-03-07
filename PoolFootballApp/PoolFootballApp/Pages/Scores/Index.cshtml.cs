88d9347e7196903a36e8898ef1814493b1dd015a
Alexandre Brochu <alexbrochu1@gmail.com>
2019-03-07 01:46:56 -0500

    Removed unused details page for matches, also added page for scores where we can see the tallies for every player of the current pools week per week, and also total

---
+++ PoolFootballApp/PoolFootballApp/Pages/Scores/Index.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PoolFootballApp.Models;
+
+namespace PoolFootballApp.Pages.Scores
+{
+	public class IndexModel : PageModel
+	{
+		private readonly NFLContext _context;
+		private readonly UserManager<IdentityUser> _userManager;
+
+		public IndexModel(NFLContext context, UserManager<IdentityUser> userManager)
+		{
+			_context = context;
+			_userManager = userManager;
+		}
+
+		[BindProperty(SupportsGet = true)]
+		public int Season { get; set; }
+
+		public Dictionary<string, string> UsersLookup { get; set; }
+
+		public async Task OnGetAsync()
+		{
+			string userId = _userManager.GetUserId(User);
+
+			Pool pool = await _context.Pools
+				.Where(p => p.UserId.Equals(userId))
+				.FirstOrDefaultAsync();
+
+			UsersLookup = new Dictionary<string, string>();
+			if (pool != null)
+			{
+				// Fetch all players in the pool
+			}
+			else
+			{
+				UsersLookup.Add(userId, _userManager.GetUserName(User));
+			}
+
+			Dictionary<int, List<int>> matchIds = new Dictionary<int, List<int>>();
+			await _context.Matches
+				.Where(m => m.Season == Season)
+				.ForEachAsync(m =>
+				{
+					if (!matchIds.ContainsKey(m.Week))
+					{
+						matchIds.Add(m.Week, new List<int>());
+					}
+					matchIds[m.Week].Add(m.Id);
+				});
+			
+			// We want to tally scores for each week in season given, default current
+			// Render a table with results week per week, and total at the bottom
+		}
+	}
+}
