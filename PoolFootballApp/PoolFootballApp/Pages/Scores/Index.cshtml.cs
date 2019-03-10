3875e2b3fae42c1270f441a50d8f19a8f1b80b3c
Alexandre Brochu <alexbrochu1@gmail.com>
2019-03-10 03:01:33 -0400

    First pass for Scores/index page is done

--- PoolFootballApp/PoolFootballApp/Pages/Scores/Index.cshtml.cs
+++ PoolFootballApp/PoolFootballApp/Pages/Scores/Index.cshtml.cs
@@ -13,6 +13,17 @@ namespace PoolFootballApp.Pages.Scores
 {
 	public class IndexModel : PageModel
 	{
+		public class UserScore
+		{
+			public string UserId;
+			public int Score;
+
+			public void CountWin()
+			{
+				Score++;
+			}
+		}
+
 		private readonly NFLContext _context;
 		private readonly IConfiguration _config;
 		private readonly UserManager<IdentityUser> _userManager;
@@ -28,7 +39,7 @@ namespace PoolFootballApp.Pages.Scores
 		public int Season { get; set; }
 
 		public Dictionary<string, string> UsersLookup { get; set; }
-		public Dictionary<int, Dictionary<string, int>> UserScores { get; set; }
+		public Dictionary<int, List<UserScore>> UserScores { get; set; }
 		public Dictionary<int, int> PossibleScores { get; set; }
 
 		public async Task OnGetAsync()
@@ -53,40 +64,39 @@ namespace PoolFootballApp.Pages.Scores
 				UsersLookup.Add(userId, _userManager.GetUserName(User));
 			}
 
-			UserScores = new Dictionary<int, Dictionary<string, int>>();
+			UserScores = new Dictionary<int, List<UserScore>>();
 			await _context.Picks
 				.Where(p => UsersLookup.ContainsKey(p.UserId))
 				.Include(p => p.Match)
 				.Where(p => p.Match.Season == Season)
-				.ForEachAsync(p =>
+				.GroupBy(p => p.Match.Week)
+				.ForEachAsync(g =>
 				{
-					if (!UserScores.ContainsKey(p.Match.Week))
+					UserScores.Add(g.Key, new List<UserScore>());
+					foreach (var id in UsersLookup.Keys)
 					{
-						UserScores.Add(p.Match.Week, new Dictionary<string, int>());
-					}
-					if (!UserScores[p.Match.Week].ContainsKey(p.UserId))
-					{
-						UserScores[p.Match.Week].Add(p.UserId, 0);
+						UserScores[g.Key].Add(new UserScore { UserId = id, Score = 0 });
 					}
 
-					if (p.Choice == MatchPick.Away && p.Match.AwayScore > p.Match.HomeScore ||
-						p.Choice == MatchPick.Home && p.Match.HomeScore > p.Match.AwayScore)
+					foreach (var pick in g.ToArray())
 					{
-						UserScores[p.Match.Week][p.UserId] += 1;
+						if (pick.Choice == MatchPick.Away && pick.Match.AwayScore > pick.Match.HomeScore ||
+							pick.Choice == MatchPick.Home && pick.Match.HomeScore > pick.Match.AwayScore)
+						{
+							UserScores[g.Key].Find(s => s.UserId.Equals(pick.UserId)).CountWin();
+						}
 					}
 				});
+
 
 			// There might be a better way to get the possible scores
 			PossibleScores = new Dictionary<int, int>();
 			await _context.Matches
 				.Where(m => m.Season == Season)
-				.ForEachAsync(m =>
+				.GroupBy(m => m.Week)
+				.ForEachAsync(g =>
 				{
-					if (!PossibleScores.ContainsKey(m.Week))
-					{
-						PossibleScores.Add(m.Week, 0);
-					}
-					PossibleScores[m.Week] += 1;
+					PossibleScores.Add(g.Key, g.Count());
 				});
 		}
 	}
