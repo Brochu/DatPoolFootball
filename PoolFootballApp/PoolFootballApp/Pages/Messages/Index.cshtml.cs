1909cf95d53cf9fa0f74f7a806f72edf7d2ee590
Alexandre Brochu <alexbrochu1@gmail.com>
2019-03-27 07:53:51 -0400

    Added pagination base for messages list

--- PoolFootballApp/PoolFootballApp/Pages/Messages/Index.cshtml.cs
+++ PoolFootballApp/PoolFootballApp/Pages/Messages/Index.cshtml.cs
@@ -12,18 +12,24 @@ namespace PoolFootballApp.Pages.Messages
 	public class IndexModel : PageModel
 	{
 		private readonly NFLContext _context;
+		private readonly int _pageCount = 10;
 
 		public IndexModel(NFLContext context)
 		{
 			_context = context;
 		}
 
+		[BindProperty(SupportsGet = true)]
+		public int pageIndex { get; set; }
+
 		public IList<Message> Message { get;set; }
 
 		public async Task OnGetAsync()
 		{
 			Message = await _context.Messages
-				.OrderBy(m => m.PostTime)
+				.OrderByDescending(m => m.PostTime)
+				.Skip(_pageCount * pageIndex)
+				.Take(_pageCount)
 				.ToListAsync();
 		}
 	}
