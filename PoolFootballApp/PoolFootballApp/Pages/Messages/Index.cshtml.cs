2facce8d8e74f21c575879993aca5d691c8a2599
Brochu <alexbrochu1@gmail.com>
2019-03-27 20:54:11 -0400

    Added filter so we only show messages from the users that are part of your pool in the forum section. If you are not in a pool, no messages are shown.

--- PoolFootballApp/PoolFootballApp/Pages/Messages/Index.cshtml.cs
+++ PoolFootballApp/PoolFootballApp/Pages/Messages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -12,25 +13,45 @@ namespace PoolFootballApp.Pages.Messages
 	public class IndexModel : PageModel
 	{
 		private readonly NFLContext _context;
+		private readonly UserManager<IdentityUser> _userManager;
 		private readonly int _pageCount = 10;
 
-		public IndexModel(NFLContext context)
+		public IndexModel(NFLContext context, UserManager<IdentityUser> userManager)
 		{
 			_context = context;
+			_userManager = userManager;
 		}
 
 		[BindProperty(SupportsGet = true)]
 		public int pageIndex { get; set; }
 
-		public IList<Message> Message { get;set; }
+		public IList<Message> Messages { get;set; }
 
 		public async Task OnGetAsync()
 		{
-			Message = await _context.Messages
-				.OrderByDescending(m => m.PostTime)
-				.Skip(_pageCount * pageIndex)
-				.Take(_pageCount)
-				.ToListAsync();
+			List<string> userIds;
+			Pool current = _context.Pools
+				.Where(p => p.UserId == _userManager.GetUserId(User))
+				.FirstOrDefault();
+
+			if (current != null)
+			{
+				userIds = await _context.Pools
+					.Where(p => p.PoolName.Equals(current.PoolName))
+					.Select(p => p.UserId)
+					.ToListAsync();
+
+				Messages = await _context.Messages
+					.Where(m => userIds.Contains(m.UserId))
+					.OrderByDescending(m => m.PostTime)
+					.Skip(_pageCount * pageIndex)
+					.Take(_pageCount)
+					.ToListAsync();
+			}
+			else
+			{
+				Messages = new List<Message>();
+			}
 		}
 	}
 }
