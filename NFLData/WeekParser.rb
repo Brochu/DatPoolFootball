19721a07d5735d0a83b100160e04637af7e143a5
Alexandre Brochu <alexbrochu1@gmail.com>
2019-09-21 14:17:41 -0400

    Use json instead of xml for nfl data

--- NFLData/WeekParser.rb
+++ NFLData/WeekParser.rb
@@ -1,38 +1,28 @@
 require 'net/http'
 require 'rexml/document'
 require 'time'
+
+require 'json'
+require 'active_support/core_ext/hash'
+
 include REXML
 
-if (ARGV.length < 2)
-    puts "NEED TO SPECIFY SEASON AND WEEK";
+if (ARGV.length < 3 || ARGV.length > 3)
+    puts "NEED TO SPECIFY SEASON, WEEK AND TYPE";
 else
     season = ARGV[0];
     week = ARGV[1];
-    type = "REG";
+    type = ARGV[2];
     #puts "Looking at season #{season} and week #{week}";
 
     uri = URI("http://www.nfl.com/ajax/scorestrip?season=#{season}&week=#{week}&seasonType=#{type}");
     #puts "Fetching from uri : #{uri}";
 
     result = Net::HTTP.get(uri);
-    xmlDoc = Document.new(result);
-    xmlDoc.get_elements("//g").each do |game|
-        home = game.attribute("h");
-        homeScore = game.attribute("hs");
-        away = game.attribute("v");
-        awayScore = game.attribute("vs");
 
-        weekDay = game.attribute("d");
-        startTime = Time.parse("#{game.attribute("t")}");
-        startTime = startTime + (60 * 60 * 12);
-
-        puts "new Match { Season = #{season}, Week = #{week}, HomeTeamId = \"#{home}\", HomeScore = #{homeScore}, AwayTeamId = \"#{away}\", AwayScore = #{awayScore}, WeekDay = Day.#{weekDay}, StartTime = DateTime.Parse(\"#{startTime.strftime("%H:%M")}\") },";
-    end
-
-    xmlDoc.get_elements("//g").each do |game|
-        home = game.attribute("h");
-        away = game.attribute("v");
-
-	puts "#{away} vs. #{home}"
+    weekData = JSON.parse(Hash.from_xml(result).to_json);
+    games = weekData["ss"]["gms"]["g"];
+    games.each do |game|
+        puts "#{game["hnn"]} #{game["hs"]} vs. #{game["vs"]} #{game["vnn"]}";
     end
 end
