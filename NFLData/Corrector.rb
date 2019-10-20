5488df9b7681eeefbd6c1cec8146b05fbc595872
Brochu <alexbrochu1@gmail.com>
2019-10-20 15:09:57 -0400

    Started writing logic for corrector script

--- NFLData/Corrector.rb
+++ NFLData/Corrector.rb
@@ -1,13 +1,46 @@
 require 'json'
 require 'active_support/core_ext/hash'
 
-if (ARGV.length < 3 || ARGV.length > 3)
-    puts "NEED TO SPECIFY SEASON, WEEK AND TYPE";
-else
+def getWeekScore(weekData)
+    return 1;
+end
+
+def printWeekScores(scoresData)
+    puts scoresData;
+end
+
+def printTotalScores(scoresArray)
+    scoresArray.each { |s| 
+        if (s != nil)
+            puts s;
+        end
+    };
+end
+
+if (ARGV.length == 3)
     season = ARGV[0];
     week = ARGV[1];
     type = ARGV[2];
 
     # GET WEEK FILE AND APPLY CORRECTIONS
-    filename = "pool-#{season}-#{week}-#{type}.txt"
+    filename = "pool-#{season}-#{week}-#{type}.json"
+    scores = getWeekScore(JSON.parse(File.read(filename)));
+    printWeekScores(scores);
+
+elsif (ARGV.length == 2)
+    season = ARGV[0];
+    type = ARGV[1];
+
+    filepaths = (1..17).map { |w| "pool-#{season}-#{w}-#{type}.json" };
+    scoresArray = filepaths.map { |file|
+        if (File.exists?(file))
+            getWeekScore(JSON.parse(File.read(file)));
+        else
+            nil;
+        end
+    };
+    printTotalScores(scoresArray);
+
+else
+    puts "NEED TO SPECIFY SEASON, WEEK AND TYPE OR SEASON AND TYPE";
 end
