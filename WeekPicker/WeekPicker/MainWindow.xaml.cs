c2bc6a08b05fef278408f616c8f1d6ebb893ae74
Brochu <alexbrochu1@gmail.com>
2019-10-27 14:26:09 -0400

    First pass on first window in picker app

--- WeekPicker/WeekPicker/MainWindow.xaml.cs
+++ WeekPicker/WeekPicker/MainWindow.xaml.cs
@@ -20,9 +20,44 @@ namespace WeekPicker
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private const int SEASON_WEEK_COUNT = 17;
+		private enum Types
+		{
+			PRE,
+			REG,
+			POST // Still need to find what value works for this
+		}
+
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void MainWindow1_Loaded(object sender, RoutedEventArgs e)
+		{
+			// Init week picker items
+			for (int i = 0; i < SEASON_WEEK_COUNT; i++)
+			{
+				WeekPick.Items.Add(new ComboBoxItem() { Content = $"semaine {i + 1}" });
+			}
+			WeekPick.SelectedIndex = 0;
+
+			// Init type picker items
+			TypePick.ItemsSource = Enum.GetValues(typeof(Types));
+			TypePick.SelectedIndex = 1;
+		}
+
+		private void GoButton_Click(object sender, RoutedEventArgs e)
+		{
+			// Web request to get the match data
+
+			if (!int.TryParse(SeasonText.Text, out int season))
+			{
+				// Could not parse season year to int
+				return;
+			}
+			int week = WeekPick.SelectedIndex + 1;
+			string type = TypePick.SelectedItem.ToString();
 		}
 	}
 }
